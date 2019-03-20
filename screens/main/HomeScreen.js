9bbee63400173af724e402a1e4fbaed277c87170
Piotr Trocki <piotr@local.com>
2019-03-20 19:42:09 +0100

    Add random people

--- screens/main/HomeScreen.js
+++ screens/main/HomeScreen.js
@@ -8,70 +8,43 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-
+import { Constants } from 'expo';
 import axios from 'axios';
-
+import Layout from '../../constants/Layout'
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
-  state = { sentences: [] }
+  state = { randomPeople: [] }
 
 
   componentDidMount() {
-    // axios.get(`https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=20`)
-    //   .then(res => {
-    //     const data = res.data
-    //     this.setState({ sentences: data });
-    //     console.log(this.state.sentences)
-    //   })
     axios.get(`https://randomuser.me/api?results=10`)
       .then(res => {
-        //console.log(res)
-        //console.log(res.data);
-        this.setState({ sentences: res.data.results });
-        //console.log(this.state.sentences)
+        this.setState({ randomPeople: res.data.results });
       })
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <FlatList
-           keyExtractor={(item, index) => index.toString()+index.toString()}
-          data={this.state.sentences}
-          renderItem={({ item }) => <View style={{backgroundColor: '#f00', height:50, width:50}}>
-
-          </View>}
+        <FlatList style = {{padding: 10}}
+          keyExtractor={(item, index) => index.toString()}
+          data={this.state.randomPeople}
+          renderItem={({ item }) => <Text style={{ padding:10}}>
+          {item.email}
+          </Text>}
         /></View>
     );
-    // return (
-    //   <View style={styles.container}>
-    //     <FlatList 
-    //     style={{ flex: 0 }}
-    //       extraData={this.state}
-    //       keyExtractor={(item, index) => index.toString()}
-    //       data={this.state.sentences}
-    //       renderItem={item => {
-
-    //         console.log(item.index);
-    //         <Text>{item.index.toString()}</Text>
-    //       }} />
-    //   </View>);
   }
 }
 const styles = StyleSheet.create({
   container: {
-    margin: 20,
+    marginTop: Constants.statusBarHeight,
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: "flex-start",
     backgroundColor: "#F5FCFF"
-  },
-  flatview: {
-    justifyContent: 'center',
-    paddingTop: 30,
-    borderRadius: 2,
   },
 });
